c9b6f9f4d054a2721025e2d696bb30119c8ce678
Muhammad Bilal Mehmood <125654503+M-BilalMehmood@users.noreply.github.com>
2023-11-27 23:15:22 +0500

    V1.0
    Started the project and checked if the classes are communicating.

---
+++ Libraries/header.h
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+//This temp code was just to check if the files are communicating with each other or not.
+
+class Tree
+{
+private:
+    int data;
+    Tree* left;
+    Tree* right;
+};
---
+++ Source/Main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"header.h"
+using namespace std;
+
+// This temp code was just to check if the files are communicating with each other or not.
+
+int main()
+{
+    Tree t;
+}
